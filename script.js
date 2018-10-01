3e05aac5964c5b1675c28d393d785174bfafca6f
Serhii <serzhyk19.92@gmail.com>
2018-10-01 11:29:46 +0200

    first commit

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var btn1 = document.getElementById("btn1");
 var btn2 = document.getElementById("btn2");
 var main = document.getElementById("main");
 var myList = document.getElementById("list");
-var ul = document.getElementsByClassName("ul");
+var ul = document.getElementsByTagName("ul");
 var story = myList.getElementsByClassName("item");
-var popup = document.getElemensByClassName("popup")[0];
+var popup = document.getElementsByClassName("popup")[0];
 var close = document.querySelector(".close");
 
 
@@ -16,12 +16,12 @@ console.log(story);
 
 function addItem(){
 	var newLi = document.createElement("li");
-	newLi.innerHTML - "Новая задача!";
+	newLi.innerHTML = "Новая задача!";
 	newLi.className = "item";
 	myList.appendChild(newLi);
 	popup.style.display = "none";
 }
-function addItem(){
+function delItem(){
 	myList.removeChild(story[0]);
 
 	if(story.length == 0){
